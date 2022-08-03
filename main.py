f5dbef9fd9a5cf5147a19144df5ffff29940bd82
Mikhail Kuznetsov <61391551+kuznetsov-m@users.noreply.github.com>
2022-08-04 02:47:59 +0300

    GUI: add device combobox

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ import sys
 import os
 import subprocess
 from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget, QPushButton, QLabel, QLineEdit, QVBoxLayout, QHBoxLayout,
-    QPlainTextEdit)
+    QPlainTextEdit, QComboBox)
 from PyQt6.QtGui import QFont
 
 
 class MainWindow(QMainWindow):
+    _args = {}
+
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle('HIDAPITester')
 
-        mono_font = QFont('Courier',13)
+        mono_font = QFont('Courier', 13)
 
         self.label = QLabel()
         self.label.setFont(mono_font)
@@ -31,29 +33,33 @@ class MainWindow(QMainWindow):
         self.output.setFont(mono_font)
         self.output.setReadOnly(True)
 
-        cmd_layout = QHBoxLayout()
-        cmd_layout.addWidget(self.label)
-        cmd_layout.addWidget(self.input)
-        cmd_layout.addWidget(self.sendBtn)
+        current_device_hl = QHBoxLayout()
+        current_device_hl.addWidget(QLabel('Device:'))
+        self.current_device_cb = QComboBox()
+        self.current_device_cb.currentText()
+        self.current_device_cb.addItems(self.get_devices_list())
+        self.current_device_cb.currentIndexChanged.connect(self.on_current_device_cb_index_changed)
+        current_device_hl.addWidget(self.current_device_cb)
 
-        layout = QVBoxLayout()
-        layout.addWidget(self.output)
-        layout.addLayout(cmd_layout)
+        cmd_hl = QHBoxLayout()
+        cmd_hl.addWidget(self.label)
+        cmd_hl.addWidget(self.input)
+        cmd_hl.addWidget(self.sendBtn)
+
+        vl = QVBoxLayout()
+        vl.addLayout(current_device_hl)
+        vl.addWidget(self.output)
+        vl.addLayout(cmd_hl)
 
         container = QWidget()
-        container.setLayout(layout)
+        container.setLayout(vl)
 
         self.setCentralWidget(container)
 
     def send_btn_slot(self):
-        command = self.input.text().split(' ')
-        p1 = subprocess.Popen(
-            [f'{os.getcwd()}/hidapitester'] + command,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            shell=False
-        )
-        c = p1.communicate()
+        command = [v for item in self._args.items() for v in item]
+        command += list(filter(self.input.text().split(' ')))
+        c = self.exec_cmd(command)
         try:
             self.output.appendPlainText(f'> hidapitester {" ".join(command)}')
 
@@ -63,6 +69,41 @@ class MainWindow(QMainWindow):
             if stderr: self.output.appendPlainText(stderr)
         except UnicodeDecodeError:
             print('UnicodeDecodeError')
+
+    def on_current_device_cb_index_changed(self):
+        text = self.current_device_cb.currentText()
+        if text == '(none)':
+            del self._args['--vidpid']
+        else:
+            self._args['--vidpid'] = text.split(':')[0]
+        self.update_args()
+
+    def get_devices_list(self):
+        c = self.exec_cmd(['--list'])
+        try:
+            stdout = c[0].decode()
+        except UnicodeDecodeError:
+            print('UnicodeDecodeError')
+        
+        return ['(none)'] + list(filter(None, stdout.split('\n'))) if stdout else []
+
+    def update_args(self):
+        text = '> hidapitester'
+        for k, v in self._args.items():
+            text += f' {k} {v}'
+        self.label.setText(text)
+
+    def exec_cmd(self, args: list) -> dict:
+        print(args)
+        p1 = subprocess.Popen(
+            [f'{os.getcwd()}/hidapitester'] + args,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            shell=False
+        )
+        return p1.communicate()
+
+
 
 
 
